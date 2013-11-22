626461d49ffd4bd7e22ecce20c280fc5c0deb702
Gergely Peidl <geri@peidl.net>
2013-11-22 13:57:12 +0100

    viewControllers updated with competitor

--- src/main/resources/assets/angularjs/app.js
+++ src/main/resources/assets/angularjs/app.js
@@ -1,29 +1,29 @@
-var app = angular.module('holdinarmApp', ['ngRoute']);
+var app = angular.module('holdinarmApp', [ 'ngRoute' ]);
 
-//This configures the routes and associates each route with a view and a controller
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/brand',
-            {
-                controller: 'ViewController',
-                templateUrl: 'assets/view/brand.html'
-            })
+// This configures the routes and associates each route with a view and a
+// controller
+app.config(function($routeProvider) {
+	$routeProvider.when('/brand', {
+		controller : 'brandViewController',
+		templateUrl : 'assets/view/brand.html'
+	})
 
-        .when('/campaign',
-            {
-                controller: 'ViewController',
-                templateUrl: 'assets/view/campaign.html'
-            })
+	.when('/campaign', {
+		controller : 'campaignViewController',
+		templateUrl : 'assets/view/campaign.html'
+	})
 
-        .when('/keyword',
-            {
-                controller: 'ViewController',
-                templateUrl: 'assets/view/keyword.html'
-            })
-        .otherwise({ redirectTo: '/brand' });
-});
+	.when('/keyword', {
+		controller : 'keywordViewController',
+		templateUrl : 'assets/view/keyword.html'
+	})
 
+	.when('/competitor', {
+		controller : 'competitorViewController',
+		templateUrl : 'assets/view/competitor.html'
+	})
 
-app.controller('ViewController', function($scope){
-	console.log("valami");
+	.otherwise({
+		redirectTo : '/brand'
+	});
 });
