1c4b5a4b86a0794b34753a56098591b777cb2469
Gergely Peidl <geri@peidl.net>
2013-11-22 18:00:25 +0100

    Modified app.js and index.html Active tab working :)

--- src/main/resources/assets/angularjs/app.js
+++ src/main/resources/assets/angularjs/app.js
@@ -28,87 +28,47 @@ app.config(function($routeProvider) {
 	});
 });
 
-
-
-var resetToDefaults = function($scope) {
+var setClasses = function($scope, next) {
 	$scope.brandClass = "default";
 	$scope.campaignClass = "default";
 	$scope.keywordClass = "default";
 	$scope.competitorClass = "default";
+
+	if (next == "brandClass")
+		$scope.brandClass = "success";
+	else if (next == "campaignClass")
+		$scope.campaignClass = "success";
+	else if (next == "keywordClass")
+		$scope.keywordClass = "success";
+	else if (next == "competitorClass")
+		$scope.competitorClass = "success";
 }
 
 app.controller('brandViewController', function($scope) {
-	resetToDefaults($scope);
+
 	$scope.brandClass = "success";
 });
 
 app.controller('campaignViewController', function($scope) {
-	resetToDefaults($scope);
+
 	$scope.campaignClass = "success";
 });
 
 app.controller('keywordViewController', function($scope) {
-	resetToDefaults($scope);
+
 	$scope.keywordClass = "success";
 });
 
 app.controller('competitorViewController', function($scope) {
-	resetToDefaults($scope);
+
 	$scope.competitorClass = "success";
 });
 
-
-/*
-var changeTab = function(valami){
-	console.log(valami);
-	
-	
-}
-*/
 app.controller('menuController', function($scope) {
-	//$scope.btnClass = "default";
-	
-    $scope.$on("$routeChangeSuccess", function (event, current, previous) {
-       //$scope.part = $route.current.activetab;
-    //console.log(current.controller);
-    //changeTab(current.controller)
-    
-    //	console.log(current.scope.campaignClass);
-    	// console.log(previous);
-     //   console.log(current);
-    });
-
-    
-    
-	
-	
-	
-//	$scope.campaignClass = "default";
-//	$scope.keywordClass = "default";
-//	$scope.competitorClass = "default";
-    
-    
-	/*
-	
-	
-	$rootScope.$on('$routeChangeSuccess', function(evt, cur, prev) {
-	 //   ...do what you have to do here, maybe set a $rootScope.path as you did
-	console.log("change!")
-	
+	$scope.$on("$routeChangeSuccess", function(angularEvent, next, current) {
+		// calls set setClasses method, and converts originalPath to
+		// /originalPath to originalPathClass
+		setClasses($scope, next.originalPath.split("/")[1] += "Class");
 	});
-	
-	*/
-	/*
-	 $scope.$on('$routeChangeSuccess', function(event,current,previous) {
-		 
-	        console.log(current.params,'current');
-	        console.log(previous.params,'previous');      
-
-	});
-*/
-
 
 });
-
-
-
